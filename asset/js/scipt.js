ab6e0e635f989af26ad46ba45ff7e3137486a4b2
xan <xanbalandrau@gmail.com>
2024-12-17 14:30:04 +0100

    first commit

--- asset/js/scipt.js
+++ asset/js/scipt.js
@@ -1,10 +1,18 @@
 // fonction pour le menu burger
+const menu = document.getElementById("navBar_menuBurger");
+const icon = document.getElementById("navBar_menuBurger_icon");
 function ouvertureMenuBurger() {
-  const menu = document.getElementById("navBar_menuBurger");
-  const icon = document.getElementById("navBar_menuBurger_icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+const fermer = document.getElementById("fermer");
+fermer.addEventListener("click", () => {
+  console.log("click");
+
+  menu.classList.remove("open");
+  icon.classList.remove("open");
+});
+
 // fonction pour ouvrir les liens des réseaux
 let toggle = document.querySelector(".toggle");
 let share = document.querySelector(".share");
